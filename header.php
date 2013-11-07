213325a6fcd5909f306da5ffe627eb0c77c91c33
Josh Broton <jbroton@vistacomm.com>
2013-11-07 12:17:56 -0600

    Create header.php * Start markup

---
+++ header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html class="no-js">
+<head>
+    <!-- METADATA -->
+    <title><?php wp_title(''); ?></title>
+    <meta name="description" content="<?php wp_title(); echo ' | '; bloginfo( 'description' ); ?>" />
+    <meta charset="utf-8">
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class('squeaky'); ?>>
+<div class="wrapper">
+    <header class="main">
+        <h1>
+            <!-- TODO: Create option in theme customizer to upload new logo -->
+            <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo('name'); ?>" /><?php bloginfo('name'); ?></a>
+        </h1>
+        <nav class="primary" role="navigation">
+            <?php wp_nav_menu( array(
+                'container' => 'ul',
+                'menu_class' => 'springtheme-main-menu',
+                'menu_id' => 'main-nav',
+                'depth' => 0,
+                'theme_location' => 'header-menu'
+            ));
+            ?>
+        </nav>
+    </header>
