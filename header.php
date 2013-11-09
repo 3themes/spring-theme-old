7f423aede36a8a3f274dd54db81646670f2afecf
Josh Broton <joshbroton@gmail.com>
2013-11-08 22:50:38 -0600

    Work on index.php and layout

--- header.php
+++ header.php
@@ -9,18 +9,18 @@
     <meta name="MobileOptimized" content="320">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('squeaky'); ?>>
 <div class="wrapper">
-    <header class="main">
+    <header class="header-main">
         <h1 class="blog-name">
             <!-- TODO: Create option in theme customizer to upload new logo -->
             <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
         </h1>
-        <nav class="primary" role="navigation">
+        <nav class="nav-main" role="navigation">
             <?php wp_nav_menu( array(
                 'container' => 'ul',
                 'menu_class' => 'springtheme-main-menu',
--- index.php
+++ index.php
@@ -1,3 +1,62 @@
 <?php get_header(); ?>
-
+	<section class="content">
+		<section class="content-main" role="main">
+			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+				<article class="post article" id="post-<?php the_ID(); ?>" <?php post_class('article clearfix'); ?> role="article">
+					<header class="article-header">
+						<h1 class="article-title">
+							<a class="article-title-link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						</h1>
+						<aside class="article-byline vcard">
+							<time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time> by <span class="author"><?php the_author_posts_link(); ?></span>
+						</aside>
+					</header>
+					<main class="article-content clearfix">
+						<?php
+						if ( has_post_thumbnail() ) {
+							the_post_thumbnail();
+						}
+						the_content();
+						?>
+					</main>
+					<footer class="article-footer">
+						<section class="article-categories">
+							Categories: <?php the_category(', '); ?>
+						</section>
+						<section class="article-tags">
+							<?php the_tags('<span class="tags-title">Tags:</span> ', ', ', ''); ?>
+						</section>
+					</footer>
+					<hr class="article-hr" />
+				</article>
+			<?php endwhile; ?>
+				<nav class="wp-prev-next">
+					<div class="prev-link">
+						<?php echo get_next_posts_link('&laquo; Older Entries') ?>
+					</div>
+					<div class="next-link">
+						<?php echo previous_posts_link('Newer Entries &raquo;') ?>
+					</div>
+				</nav>
+				<?php wp_link_pages(); ?>
+			<?php else : ?>
+				<article class="post-404">
+					<header class="article-header">
+						<h1 class="article-title">
+							Oops! Post not found.
+						</h1>
+					</header>
+					<section class="article-content">
+						Something has gone very, very, VERY wrong. Can you make sure you clicked on the right thing?
+					</section>
+					<footer class="article-footer">
+						<p>
+							You've gotten this error message from the index.php file in the template.
+						</p>
+					</footer>
+				</article>
+			<?php endif; ?>
+		</section>
+		<?php //get_sidebar(); ?>
+	</section>
 <?php get_footer(); ?>
