dbff3f45a7598c31e5268078f6861eddff0b2026
Dan Beil <dan.beil@blueearth.net>
2013-11-19 22:08:03 -0600

    js fix for .first, not perfect

--- js/spring.js
+++ js/spring.js
@@ -1,4 +1,14 @@
 jQuery(window).load(function(){
 		var $ = jQuery;
-		$('.columns-1, columns-2').first().addClass('first');
+		$('.columns-1').first().addClass('first');
+		$('.columns-2').first().addClass('first');
+		$('.columns-3').first().addClass('first');
+		$('.columns-4').first().addClass('first');
+		$('.columns-5').first().addClass('first');
+		$('.columns-6').first().addClass('first');
+		$('.columns-7').first().addClass('first');
+		$('.columns-8').first().addClass('first');
+		$('.columns-9').first().addClass('first');
+		$('.columns-10').first().addClass('first');
+		$('.columns-11').first().addClass('first');
 });
